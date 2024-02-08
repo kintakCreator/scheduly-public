c7a3d11b292e42bfb4364e1e4ec7b37ed06b8d54
Araragi Yusuke <kinas5727@gmail.com>
2024-02-08 16:42:45 +0300

    updated marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -31,11 +31,6 @@ const MarketingPage = () => {
                 "flex items-center justify-center flex-col",
                 headingFont.className,
             )}>
-                <div className="mb-4 flex items-center border shadow-sm p-4 
-                bg-amber-100 text-amber-700 rounded-full uppercase">
-                    <Medal className="h-6 w-6 mr-2"/>
-                    The best task managment
-                </div>
                 <h1 className="text-3xl md:text-6xl text-center text-neutral-800
                 mb-6">
                     Welcome to the
